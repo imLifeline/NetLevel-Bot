12c1ce7af32630cc50b1e8d81e4c0f57de96ceaf
imLifeline <50486405+imLifeline@users.noreply.github.com>
2024-02-16 04:03:56 +0800

    Update configure.ts

--- src/commands/Owner/configure.ts
+++ src/commands/Owner/configure.ts
@@ -55,7 +55,7 @@ export default new NetLevelBotCommand({
         dm_permission: false
     },
     options: {
-        guildOwnerOnly: true
+        guildOwnerOnly: false
     },
     callback: async (client, interaction) => {
 
